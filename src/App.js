ff41c84cee72abfe7c0d74f93e97ac24242e0e04
Choozii <jiyechoo@gmail.com>
2024-04-23 22:28:36 +0900

    성능 최적화

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, {lazy, Suspense} from 'react'
 import { Switch, Route } from 'react-router-dom'
 import './App.css'
-import ListPage from './pages/ListPage/index'
-import ViewPage from './pages/ViewPage/index'
 
 function App() {
+
+  const ListPage = lazy(()=>import('./pages/ListPage/index'));
+  const ViewPage = lazy(()=>import('./pages/ViewPage/index'));
+
   return (
     <div className="App">
+      <Suspense fallback={<></>}>
       <Switch>
         <Route path="/" component={ListPage} exact />
         <Route path="/view/:id" component={ViewPage} exact />
       </Switch>
+      </Suspense>
     </div>
   )
 }
